e718cb28d525e7b9aee000417161f520e5ea5b71
taha <127975821+taha127@users.noreply.github.com>
2025-04-13 14:59:54 +0330

    Add Allowed Host

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'torkan-reserve.onrender.com',
+]
 
 
 # Application definition
